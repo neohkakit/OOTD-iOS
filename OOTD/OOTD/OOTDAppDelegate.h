e55952dc2217be5c117f3988472b8757a6379158
Ryne Cheow <rynec92@hotmail.com>
2013-05-26 17:07:37 +0800

    proj files

---
+++ OOTD/OOTD/OOTDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OOTDAppDelegate.h
+//  OOTD
+//
+//  Created by Matthias Ryne Cheow on 5/26/13.
+//  Copyright (c) 2013 OOTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OOTDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OOTD/OOTD/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OOTD
+//
+//  Created by Matthias Ryne Cheow on 5/26/13.
+//  Copyright (c) 2013 OOTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OOTDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OOTDAppDelegate class]));
+    }
+}
